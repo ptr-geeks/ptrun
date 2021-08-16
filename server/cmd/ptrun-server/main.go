2ae9030e294c6ab60ec8640b41f5d2dd9d043e77
mytja <mytja@protonmail.com>
2021-08-16 21:44:14 +0200

    Added Dockerfile, which fully works & an example of HTTP API and Websocket running in same thread & added a vendor folder

--- server/cmd/ptrun-server/main.go
+++ server/cmd/ptrun-server/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("PTRun Backend is running!")
+	time.Sleep(8 * time.Second)
 }
--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/ptr-geeks/ptrun/server
 
 go 1.15
+
+require github.com/gorilla/websocket v1.4.2
