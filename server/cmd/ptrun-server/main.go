04b3d5992a4049e69d846cd153b1a59ea7f4c069
filips123 <filip.stamcar@hotmail.com>
2021-08-18 11:43:39 +0200

    Add support to set server config using flags

--- server/cmd/ptrun-server/main.go
+++ server/cmd/ptrun-server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"net/http"
 	"os"
 	"os/signal"
@@ -20,10 +21,21 @@ var (
 )
 
 func main() {
-	// Switch with zap.NewProduction() when needed
-	// or even better, add a flag to switch this as needed.
-	// Example: ./ptrun-server --debug
-	logger, err := zap.NewDevelopment()
+	var debug = flag.Bool("debug", false, "enable debug mode")
+	var host = flag.String("host", "0.0.0.0", "set server host")
+	var port = flag.String("port", "8080", "set server port")
+	var path = flag.String("path", "/ws", "set server WS path")
+	flag.Parse()
+
+	var logger *zap.Logger
+	var err error
+
+	if *debug {
+		logger, err = zap.NewDevelopment()
+	} else {
+		logger, err = zap.NewProduction()
+	}
+
 	if err != nil {
 		panic(err)
 	}
@@ -33,21 +45,19 @@ func main() {
 	server = ws.NewServer(logger)
 	go server.Run()
 
-	// TODO: We should allow some overrides by passing parameters to our executable
-	// Example: ./ptr-server --port 8080 --path "/ws"
 	sugared.Infow("starting websocket endpoint",
-		"addr", "0.0.0.0",
-		"port", 8080,
-		"path", "/ws")
+		"host", host,
+		"port", port,
+		"path", path)
 
 	mux := goji.NewMux()
-	mux.HandleFunc(pat.Get("/ws"), func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc(pat.Get(*path), func(w http.ResponseWriter, r *http.Request) {
 		server.Connect(w, r)
 	})
 
 	srv := &http.Server{
 		Handler: mux,
-		Addr:    "0.0.0.0:8080",
+		Addr:    *host + ":" + *port,
 		// These should probably be moved under internal/const
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
