4ad7bc4316d04263ed79b824b271fd93be215c60
Grogl <ziga2003.groselj@gmail.com>
2021-08-19 11:00:01 +0200

    Fixed lint problems

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import { Terrain } from './objects/terrain';
 
 import grassTileImg from './assets/grassTile.jpg';
 import dirtTileImg from './assets/dirtTile.jpg';
-import backgroundImg from './assets/oblakiBG.jpg'
+import backgroundImg from './assets/oblakiBG.jpg';
 import { Player } from './objects/player';
 import playerImg from './assets/player_image.png';
 
@@ -12,10 +12,9 @@ class Game extends Phaser.Scene {
     constructor() {
         super();
 
-        this.cursors = {}
-        this.player = null
-        this.wasd = {}
-
+        this.cursors = {};
+        this.player = null;
+        this.wasd = {};
     }
 
     preload() {
@@ -33,7 +32,7 @@ class Game extends Phaser.Scene {
         this.cameras.main.startFollow(this.player, false, 1, 1, -350, 200);
         this.bg.setScrollFactor(0);
         this.add.existing(new Terrain(this, 0, 0, 'grassTile', 'dirtTile', this.player));
-        
+
 
         this.cursors = this.input.keyboard.createCursorKeys();
         this.wasd = this.input.keyboard.addKeys('W,S,A,D');
@@ -45,29 +44,30 @@ class Game extends Phaser.Scene {
     }
 
 
+    handlePlayerMove() {
+        this.player.body.setVelocity(0);
 
-    handlePlayerMove(){
-        if (this.cursors.left.isDown || this.wasd.A.isDown){
+        if (this.cursors.left.isDown || this.wasd.A.isDown) {
             this.player.body.setVelocityX(-300);
-        } else if (this.cursors.right.isDown || this.wasd.D.isDown){
+        } else if (this.cursors.right.isDown || this.wasd.D.isDown) {
             this.player.body.setVelocityX(300);
-        } else {
-            this.player.body.setVelocityX(0);
         }
-        if(this.cursors.up.isDown && this.player.body.touching.down){
-            this.player.body.setVelocityY(-800);
+        if (this.cursors.down.isDown || this.wasd.S.isDown) {
+            this.player.body.setVelocityY(300);
+        } else if (this.cursors.up.isDown || this.wasd.W.isDown) {
+            this.player.body.setVelocityY(-300);
         }
     }
 }
 
 const config = {
     type: Phaser.AUTO,
-    parent: "game",
+    parent: 'game',
     width: window.innerWidth,
     height: window.innerHeight,
     scale: {
         autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
-        mode: Phaser.Scale.NONE, 
+        mode: Phaser.Scale.NONE,
     },
     physics: {
         default: 'arcade',
@@ -81,4 +81,4 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-window.addEventListener("resize", () => game.scale.resize(window.innerWidth, window.innerHeight));
+window.addEventListener('resize', () => game.scale.resize(window.innerWidth, window.innerHeight));
--- client/src/objects/player.js
+++ client/src/objects/player.js
@@ -1,12 +1,10 @@
 import Phaser from 'phaser';
 
 export class Player extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, player ) {
-      super(scene, x, y, player);
-      scene.physics.add.existing(this); 
-      this.setScale(0.03);
-      this.body.gravity.y = 1000;
-
+    constructor(scene, x, y, player) {
+        super(scene, x, y, player);
+        scene.physics.add.existing(this);
+        this.setScale(0.03);
     }
 }
 
