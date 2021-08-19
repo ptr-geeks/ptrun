ade1ce3d5ca3d41304e8e983c8f73647f7ec25db
filips123 <filip.stamcar@hotmail.com>
2021-08-19 09:50:19 +0200

    Fix requested changes

--- server/cmd/ptrun-server/main.go
+++ server/cmd/ptrun-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
@@ -20,17 +21,25 @@ var (
 	server ws.Server
 )
 
+type ServerConfig struct {
+	Debug bool
+	Host  string
+	Port  string
+	Path  string
+}
+
 func main() {
-	var debug = flag.Bool("debug", false, "enable debug mode")
-	var host = flag.String("host", "0.0.0.0", "set server host")
-	var port = flag.String("port", "8080", "set server port")
-	var path = flag.String("path", "/ws", "set server WS path")
+	config := ServerConfig{}
+	flag.BoolVar(&config.Debug, "debug", false, "enable debug mode")
+	flag.StringVar(&config.Host, "host", "0.0.0.0", "set server host")
+	flag.StringVar(&config.Port, "port", "8080", "set server port")
+	flag.StringVar(&config.Path, "path", "/ws", "set server WS path")
 	flag.Parse()
 
 	var logger *zap.Logger
 	var err error
 
-	if *debug {
+	if config.Debug {
 		logger, err = zap.NewDevelopment()
 	} else {
 		logger, err = zap.NewProduction()
@@ -46,18 +55,18 @@ func main() {
 	go server.Run()
 
 	sugared.Infow("starting websocket endpoint",
-		"host", host,
-		"port", port,
-		"path", path)
+		"host", config.Host,
+		"port", config.Port,
+		"path", config.Path)
 
 	mux := goji.NewMux()
-	mux.HandleFunc(pat.Get(*path), func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc(pat.Get(config.Path), func(w http.ResponseWriter, r *http.Request) {
 		server.Connect(w, r)
 	})
 
 	srv := &http.Server{
 		Handler: mux,
-		Addr:    *host + ":" + *port,
+		Addr:    fmt.Sprintf("%s:%s", config.Host, config.Port),
 		// These should probably be moved under internal/const
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
