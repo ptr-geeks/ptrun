d42987e48f6dd1e07a75ea1aa62eff87fb1152cb
Arne Salobir <arne.salobir@gmail.com>
2021-08-19 11:47:14 +0200

    Spremenil lokacijo konstant iz main.go v consts.go. client.go želi github.com/ptr-geeks/ptrun/server/internal/messages, samo ne dobi

--- server/internal/consts/consts.go
+++ server/internal/consts/consts.go
@@ -1,5 +1,12 @@
 package consts
 
+import (
+	"time"
+)
+
 const (
 	ChanBufferSize = 200
+	WriteTimeout   = 15 * time.Second
+	ReadTimeout    = 15 * time.Second
+	IdleTimeout    = 15 * time.Second
 )
