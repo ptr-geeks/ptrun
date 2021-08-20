2ddba7aca4a58ccc38bd167173a98549b717ffc3
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-08-20 09:07:27 +0200

    Update background.js

--- client/src/objects/background.js
+++ client/src/objects/background.js
@@ -5,6 +5,7 @@ export class Background extends Phaser.GameObjects.TileSprite {
         super(scene, x, y, width, height, texture);
         this.setOrigin(0);
         this.setScrollFactor(0, 0);
+        this.setScale(4);
     }
 
     update() {
