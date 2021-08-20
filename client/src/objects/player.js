7fdbb49763051575ddc3118e41ffdf1511f0f85c
David Mohar <david.mohar@shekaj.si>
2021-08-20 11:23:04 +0200

    merged all outstanding PR(s) into a single one to avoid constant conflicts

--- client/src/objects/player.js
+++ client/src/objects/player.js
@@ -1,10 +1,57 @@
 import Phaser from 'phaser';
 
-export class Player extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, player) {
-        super(scene, x, y, player);
-        scene.physics.add.existing(this);
-        this.setScale(0.03);
+export class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture) {
+        super(scene, x, y, texture);
+
+        this.scene.physics.world.enable(this);
+        this.scene.add.existing(this);
+
+        // Random color
+        var color = new Phaser.Display.Color();
+        color.random(50, 200);
+        this.setTint(color.color);
+
+        //this.setScale(0.03);
+        this.setDepth(50);
+        this.setScale(0.15);
+        this.body
+            .setSize(630, 500, true)
+            .setOffset(500, 280);
+
+        this.runAnimationFrames = this.anims.generateFrameNames('player', { prefix: 'frame', start: 1, end: 6 });
+        this.anims.create({
+            key: 'run',
+            paused: true,
+            frames: this.runAnimationFrames,
+            frameRate: 15,
+            repeat: -1,
+        });
+    }
+
+    move(x, y, dx, dy) {
+        this.setPosition(x, y);
+        this.setVelocityX(dx);
+
+        if (dy != 0) {
+            this.setVelocityY(dy);
+        }
+
+        if (dx < 0) {
+            this.flipX = true;
+            this.body.setOffset(790, 280);
+        } else if (dx > 0) {
+            this.flipX = false;
+            this.body.setOffset(500, 280);
+        }
+
+        if (!this.anims?.isPlaying && dx != 0) {
+            this.play({ key: 'run' });
+        }
+
+        if (dx == 0 || !this.body.touching.down) {
+            this.anims.stop();
+        }
     }
 }
 
